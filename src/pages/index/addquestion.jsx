81ac54bad39fdbe9275c58a06df02e148cb4b8b9
shiqiangdva <18840802108@163.com>
2019-10-17 11:19:09 +0800

    问答小程序/H5完成(就一个页面进行提问,将提问数据进行持久化缓存,组件化提问model弹框,数据流及相关基础知识点总结)

--- src/pages/index/addquestion.jsx
+++ src/pages/index/addquestion.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { Component } from "@tarojs/taro";
+import Taro, { Component } from "@tarojs/taro";
 import { View, Input, Textarea, Button } from "@tarojs/components";
 import Dialog from "./dialog";
 import './addquestion.less';
@@ -15,16 +15,40 @@ export default class AddQuestion extends Component {
         this.props.onCloseQuestion && this.props.onCloseQuestion();
     }
 
+    btnOk() {
+        // 点击确定时, 采集数据, 关闭窗体传递数据
+        console.log('点击了确定按钮!');
+        if(this.state.title && this.state.desc) {
+            this.props.onReveiveQuestion && this.props.onReveiveQuestion(this.state);
+        } else {
+            Taro.showToast({title: '请输入标题或者描述', icon: 'none', duration:  2000})
+        }
+    }
+
+    changeTitle(e) {
+        console.log(e);
+        this.setState({
+            title: e.target.value
+        })
+    }
+
+    changeDes(e) {
+        console.log(e);
+        this.setState({
+            desc: e.target.value
+        })
+    }
+
     render() {
         return (
             <Dialog>
                 <View className='add-question'>
                     <View className='question-body'>
-                        <Input className='question-title' placeholder='请输入您问题的标题' />
-                        <Textarea className='question-desc' placeholder='请输入您问题的描述' value={this.state.desc}></Textarea>
+                        <Input focus onInput={this.changeTitle.bind(this)} className='question-title' placeholder='请输入您问题的标题' />
+                        <Textarea onInput={this.changeDes.bind(this)} className='question-desc' placeholder='请输入您问题的描述' ></Textarea>
                         <View className='btn-group'>
-                            <Button className='btn-question ok'>确定</Button>
-                            <Button onClick={this.btnCancel.bind(this)} className='btn-question cancel'>取消</Button>
+                            <Button onClick={this.btnOk.bind(this)} className='btn-questions ok'>确定</Button>
+                            <Button onClick={this.btnCancel.bind(this)} className='btn-questions cancel'>取消</Button>
                         </View>
                     </View>
                 </View>
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,7 +1,28 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Button } from '@tarojs/components'
+import { View, Button, Text, Image } from '@tarojs/components'
 import './index.less'
 import AddQuestion from './addquestion'
+
+function getStore(key) {
+  let str = Taro.getStorageSync(key);
+  if (!str) {
+    return [];
+  }
+  return JSON.parse(str);
+}
+
+function setStore(key, obj) {
+  let str = obj;
+  if (typeof obj == 'object') {
+    str = JSON.stringify(obj)
+  }
+  Taro.setStorageSync(key, str);
+}
+
+// let arr = getStore('question').map((item, index) => {
+//   return { id: parseInt(Math.random()*10000) , ...item }
+// })
+// setStore('question', arr);
 
 export default class Index extends Component {
 
@@ -11,39 +32,119 @@ export default class Index extends Component {
   }
 
   state = {
-    showQuestionModel: false
+    showQuestionModel: false,
+    questionList: getStore('question')
   }
 
-  componentWillMount () { }
+  componentWillMount() { }
 
-  componentDidMount () { }
+  componentDidMount() { }
 
-  componentWillUnmount () { }
+  componentWillUnmount() { }
 
-  componentDidShow () { }
+  componentDidShow() { }
 
-  componentDidHide () { }
+  componentDidHide() { }
 
-      // 提问事件方法
-      addQuestion() {
-        this.setState({
-          showQuestionModel: true
-        })
+  // 提问事件方法
+  addQuestion() {
+    this.setState({
+      showQuestionModel: true
+    })
+  }
+
+  closeQuestion() {
+    this.setState({
+      showQuestionModel: false
+    })
+  }
+
+  reveiveQuestion(options) {
+    let { questionList } = this.state;
+    questionList.push({ id: parseInt(Math.random()*10000), ...options});
+    this.setState({
+      questionList: questionList
+    }, () => {
+      console.log(this.state.questionList);
+      setStore('question', this.state.questionList)
+    })
+    this.closeQuestion();
+  }
+
+  // 投票
+  addVote(item) {
+    let {questionList} = this.state;
+    if(item) {
+      item.vote = item.vote ? item.vote + 1 : 1;
+    }
+    let newArr = questionList.map(listItem => {
+      if(listItem.id === item.id) {
+        listItem = item;
       }
-    
-      closeQuestion() {
-        this.setState({
-          showQuestionModel: false
-        })
-      }
+      return listItem;
+    })
+    this.setState({
+      questionList: newArr 
+    }, () => {
+      // 持久层更新
+      setStore('question', this.state.questionList);
+    })
+  }
 
-  render () {
+  // 减票
+  cutVote(item) {
+    let {questionList} = this.state;
+    if(item) {
+      item.vote = item.vote&&item.vote>0 ? item.vote - 1 : 0;
+    }
+    let newArr = questionList.map(tmp => {
+      if(tmp.id === item.id) {
+        tmp = item;
+      }
+      return tmp;
+    });
+    this.setState({
+      questionList: newArr
+    }, () => {
+      // 持久层更新
+      setStore('question', this.state.questionList);
+    })
+  }
+
+  render() {
+    let { questionList } = this.state;
+    // 排序 通过sort方法根据点赞多少进行排序
+    let myList = questionList.sort((a, b) => {
+      return b.vote - a.vote;
+      // a.vote < b.vote
+    })
+    console.log(myList);
+
     return (
       <View className='index'>
         <View className='title'>Taro问答实例</View>
-        {this.state.showQuestionModel ? <AddQuestion onCloseQuestion={this.closeQuestion.bind(this)} /> : null}
+        <View className='question-list'>
+          {
+            myList.map((item, index) => {
+              return (
+                <View className='question' key={index+1}>
+                  <View className='question-left'>
+                    <View className='question-title'>{item.title}</View>
+                    <View className='question-desc'>{item.desc}</View>
+                  </View>
 
-        <Button onClick={this.addQuestion} className='btn-question'>提问</Button>
+                  <View className='question-right'>
+                    <Image className='img' onClick={this.addVote.bind(this, item)} src={require('../../img/yes.png')} />
+                    <Text>{item.vote?item.vote:0}</Text>
+                    <Image className='img' onClick={this.cutVote.bind(this, item)} src={require('../../img/no.png')} />
+                  </View>
+                </View>)
+            })
+          }
+        </View>
+        {this.state.showQuestionModel ? <AddQuestion onReveiveQuestion={this.reveiveQuestion.bind(this)} onCloseQuestion={this.closeQuestion.bind(this)} /> : null}
+
+        <Button onClick={this.addQuestion.bind(this)} className='btn-question'>提问</Button>
       </View>
     )
   }
